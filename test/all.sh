f546732c41fc825f3a6961d14af22d372f41a389
tuckerrrrrrrrrr <tucker1014@hotmail.com>
2021-06-06 16:35:52 -0700

    add all.sh and loosely enforce the cwd

---
+++ test/all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+err() {
+	echo "$0: $1" 2>&1
+	exit 1
+}
+
+if [ "$(basename $(pwd))" != "test" ]; then
+	err "must be run in test/"
+fi
+
+cd ../
+make
+cd test
+
+for filename in $(ls -1 *.c); do
+	packet_name=$(echo "$filename" | sed 's/\.c//g')
+	./build.sh "$packet_name"
+done
--- test/build.sh
+++ test/build.sh
@@ -15,6 +15,10 @@ err () {
 	exit 1
 }
 
+if [ "$(basename $(pwd))" != "test" ]; then
+	err "must be run in test/"
+fi
+
 if [ -z "$packet_name" ]; then
 	err "empty test name"
 elif ! [ -r "$packet_name.c" ]; then
@@ -23,10 +27,10 @@ elif ! [ -r "$packet_name.bin" ]; then
 	err "\"$packet_name.bin\" doesn't exist / isn't readable"
 fi
 
-cd ../
-make
-./pc examples/$packet_name.packet > /tmp/$packet_name.h
-cd test
+if ! [ -x "../pc" ]; then
+	err "../pc doesn't exist / isn't executable"
+fi
+../pc ../examples/$packet_name.packet > /tmp/$packet_name.h
 
 gcc -g -I/tmp -o /tmp/$packet_name $packet_name.c $CHOWDER_DIR/{conn,packet,nbt,player}.c $CHOWDER_DIR/include/linked_list.c -lssl -lcrypto
 
