1d86320dc57374f90aa96f527c7da45b2c97dbf6
tuckerrrrrrrrrr <tucker1014@hotmail.com>
2021-06-06 16:07:56 -0700

    run all tests with build.sh
    build.sh takes the packet name as an arg now, so every packet
    test can be run through build.sh.

--- test/build.sh
+++ test/build.sh
@@ -4,19 +4,34 @@
 # than just build
 
 # TODO: terminate when make or gcc fail
-# TODO: take a packet name, eg. "player_info", and plug that name in below
-#       so this can be used for any test
+
+CHOWDER_DIR="$(awk -F'= ' '{print $2}' ../config.mk)"
+
+script_name="$0"
+packet_name="$1"
+
+err () {
+	echo "$script_name: $1" 2>&1
+	exit 1
+}
+
+if [ -z "$packet_name" ]; then
+	err "empty test name"
+elif ! [ -r "$packet_name.c" ]; then
+	err "\"$packet_name.c\" doesn't exist / isn't readable"
+elif ! [ -r "$packet_name.bin" ]; then
+	err "\"$packet_name.bin\" doesn't exist / isn't readable"
+fi
 
 cd ../
 make
-./pc examples/player_info.packet > /tmp/player_info.h
+./pc examples/$packet_name.packet > /tmp/$packet_name.h
 cd test
 
-CHOWDER_DIR="$(awk -F'= ' '{print $2}' ../config.mk)"
-gcc -g -I/tmp -o /tmp/player_info player_info.c $CHOWDER_DIR/{conn,packet,nbt,player}.c $CHOWDER_DIR/include/linked_list.c -lssl -lcrypto
+gcc -g -I/tmp -o /tmp/$packet_name $packet_name.c $CHOWDER_DIR/{conn,packet,nbt,player}.c $CHOWDER_DIR/include/linked_list.c -lssl -lcrypto
 
-packet_file_path="$(/tmp/player_info)"
-diff player_info.bin $packet_file_path || exit 1
+packet_file_path="$(/tmp/$packet_name)"
+diff $packet_name.bin $packet_file_path || exit 1
 
 rm $packet_file_path
-rm /tmp/player_info
+rm /tmp/$packet_name
